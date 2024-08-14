8a60b35aae56f5710a3c2e8aafe44e8dc4841945
vinu-ux <vineethes8@gmail.com>
2024-08-14 22:07:33 +0530

    added

---
+++ mail.js
@@ -0,0 +1,52 @@
+function sendEmail(){
+    console.log('hi');
+    var param={
+        fullName: document.getElementById('firstName').value + ' '+ document.getElementById('lastName').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        message: document.getElementById('message').value,
+    }
+
+    const serviceId='service_ild3j2l'
+    const templateId='template_0m74iei'
+
+    emailjs.send(serviceId,templateId,param).then(
+        (res)=>{
+            console.log(res);
+            document.getElementById('firstName').value =''
+            document.getElementById('lastName').value =''
+        document.getElementById('email').value=''
+        document.getElementById('phone').value=''
+        document.getElementById('message').value=''
+            alert("successfully submit details")
+        }
+    ).catch(err=>{console.log(err);})
+   
+}
+function sendMessage(){
+    console.log('hi');
+    var param={
+        name: document.getElementById('firstName').value + ' '+ document.getElementById('lastName').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value,
+    }
+
+    const serviceId='service_ild3j2l'
+    const templateId='template_64idxyv'
+
+    emailjs.send(serviceId,templateId,param).then(
+        (res)=>{
+            console.log(res);
+            document.getElementById('firstName').value =''
+            document.getElementById('lastName').value =''
+        document.getElementById('email').value=''
+        document.getElementById('subject').value=''
+        document.getElementById('message').value=''
+            alert("successfully submit details")
+        }
+    ).catch(err=>{console.log(err);})
+   
+}
+
+ 
